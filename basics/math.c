6365cc1873a60100abe08b405913ee3dc1102fff
joel06rob <joel.roberts2006@gmail.com>
2026-01-31 21:11:23 +0000

    31.1

---
+++ basics/math.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+
+    float x = 3;
+
+    //Squareroot, Powers, Round, Round Up Ceil, Round Down Floor, Absolute value (negatives), log, sincostan, 
+
+    //x = sqrt(x);
+    //x = pow(x, 2);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x = abs(x);
+    //x = log(x);
+    //x = sin(x);
+    //x = cod(x);
+    //x = tan(x);
+
+    printf("%f", x);
+
+    return 0;
+}
