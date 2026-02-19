39c7e2a09a5e39fec3c567698131e46d2e22ac0d
joel06rob <joel.roberts2006@gmail.com>
2026-02-19 21:52:52 +0000

    19.02

--- projects/tempconverter.c
+++ projects/tempconverter.c
@@ -5,6 +5,7 @@ int main(){
 
     char name[50] = "";
     char userselection;
+    float currenttemp = 0.0f;
     float currenttemp_celsius = 0.0f;
     float currenttemp_fahrenheit = 0.0f;
 
@@ -15,19 +16,33 @@ int main(){
     printf("Please select your temperature measurement unit\n C - Celsius \n F - Fahrenheit");
     scanf(" %c", &userselection);
 
-    //determine measurements
+    //Determine measurements
     switch(userselection){
         case 'C':
         case 'c':
-            printf("Celsius selected");
+            userselection = 'C';
+            printf("Celsius selected\n");
             break;
         case 'F':
         case 'f':
-            printf("Fahrenheit selected");
+            userselection = 'F';
+            printf("Fahrenheit selected\n");
             break;
         default:
-            printf("Error - Please enter a valid selection (C or F)");
+            printf("Error - Please enter a valid selection (C or F)\n");
     }
+
+    //Input temperature
+    printf("Please input the current temperature in %c :", userselection);
+    scanf(" %f", &currenttemp);
+
+    //TODO: Add validation
+
+    //Outputs (Menu Selection)
+    printf("==============================\nWELCOME TO THE TEMP CONVERTER %s\n==============================\n", name);
+    printf("CURRENT TEMPERATURE (%c): %.1f\n", userselection, currenttemp);
+    printf("SELECT OPTION:\n>CONVERT TEMP\n>CHANGE TEMP\n>CHECK TEMP\n");
+
 
 
     return 0;
